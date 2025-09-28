3240690a12102b5e103fd56b9d6b92efab6c8508
Jheisson Orlando Cabezas Vera <jheissoncabezas@gmail.com>
2025-09-28 12:35:37 -0500

    Update api.js - Se agrega const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080';

--- front/src/services/api.js
+++ front/src/services/api.js
@@ -1,4 +1,5 @@
-const BASE_URL = 'http://localhost:8080'; 
+//const BASE_URL = 'http://localhost:8080';
+const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8080';
 
 class ApiService {
   constructor() {
